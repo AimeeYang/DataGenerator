ae42955d388fe5a45c31adb1543cc077e9085350
AimeeYang <1014279248@qq.com>
2016-01-29 16:51:53 +0800

    people info generator  price generator

--- src/main/scala/sparkdemo/datagenerator/PriceGenerator.scala
+++ src/main/scala/sparkdemo/datagenerator/PriceGenerator.scala
@@ -1,5 +1,8 @@
 package sparkdemo.datagenerator
-
+/**
+  * Created by ayang on 2016/1/29.
+  * refer: https://www.ibm.com/developerworks/cn/opensource/os-cn-spark-practice1/
+  */
 import java.io.FileWriter
 import java.io.File
 import scala.util.Random
@@ -9,7 +12,7 @@ object PriceGenerator{
     val writer = new FileWriter(new File("F:\\pricetest.txt"),false)
     val rand=new Random()
     for(i <- 1 to 100000){
-      writer.write(i+" "+(Random.nextInt(100).toDouble +Random.nextDouble()))
+      writer.write(i+" "+Random.nextInt(100))
       writer.write(System.getProperty("line.separator"))
     }
     writer.flush()
